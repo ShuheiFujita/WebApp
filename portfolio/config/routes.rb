1855caaf8442498393b85ecd5c3f7ecd84b3b49c
shu <dstopimagining@gmail.com>
2015-12-26 23:23:40 +0900

    Home,About,Blogの画面遷移を実装

--- portfolio/config/routes.rb
+++ portfolio/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+    root 'home#index'
+    get "about", :to => 'about#index'
+   # get 'about/index'
+    get "blog", :to => 'blog#index'
+   # get 'blog/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
